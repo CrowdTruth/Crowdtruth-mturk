2a23ff317e9dc7d1f0ab7b574a9169dca9c7dc78
Carlos Martinez <c.martinez@esciencecenter.nl>
2014-09-23 14:28:33 +0200

    Add missing comma in config

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-        'rooturl' => 'https://mechanicalturk.sandbox.amazonaws.com/'
+        'rooturl' => 'https://mechanicalturk.sandbox.amazonaws.com/',
         'sandboxurl' => 'https://mechanicalturk.sandbox.amazonaws.com/',
         'accesskey' => '',
         'secretkey' => ''
